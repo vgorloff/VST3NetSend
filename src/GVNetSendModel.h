39e8fa9d08cfcb0bbf94689612a1b6d1a159adf5
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2013-01-22 19:51:03 +0200

    UI bindings configured.

---
+++ src/GVNetSendModel.h
@@ -0,0 +1,13 @@
+//
+//  GVNetSendModel.h
+//  VST3NetSend
+//
+//  Created by Volodymyr Gorlov on 1/22/13.
+//  Copyright (c) 2013 Vlad Gorloff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GVNetSendModel : NSObject
+
+@end
---
+++ src/GVNetSendModel.m
@@ -0,0 +1,13 @@
+//
+//  GVNetSendModel.m
+//  VST3NetSend
+//
+//  Created by Volodymyr Gorlov on 1/22/13.
+//  Copyright (c) 2013 Vlad Gorloff. All rights reserved.
+//
+
+#import "GVNetSendModel.h"
+
+@implementation GVNetSendModel
+
+@end
