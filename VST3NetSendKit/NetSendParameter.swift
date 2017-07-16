838d8b5f15678b4e1ca98da47f969a90da4bf907
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2017-07-16 15:39:40 +0200

    Restored bindings.

---
+++ VST3NetSendKit/NetSendParameter.swift
@@ -0,0 +1,13 @@
+//
+//  NetSendParameter.swift
+//  VST3NetSendKit
+//
+//  Created by Vlad Gorlov on 16.07.17.
+//  Copyright © 2017 Vlad Gorlov. All rights reserved.
+//
+
+import Foundation
+
+@objc public enum NetSendParameter: Int {
+   case dataFormat, port, bonjourName, password, connectionFlag
+}
