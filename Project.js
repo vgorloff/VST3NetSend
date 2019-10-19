9e29483f2f8d58dc9ffdc069b17812b2b9a83cfb
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2019-10-19 23:00:09 +0200

    Project configuration update.

--- Project.js
+++ Project.js
@@ -1,45 +1,43 @@
-import * as FileSystem from 'fs';
-import * as Path from 'path';
-import * as WL from "wl-scripting";
+const fs = require('fs');
+const Path = require('path');
+const AbstractProject = require('wl-scripting').AbstractProject;
+const XcodeBuilder = require('wl-scripting').XcodeBuilder;
+const FileSystem = require('wl-scripting').FileSystem;
+const execute = require('wl-scripting').Functions.execute;
+const puts = require('wl-scripting').Functions.puts;
 
-class Project extends WL.AbstractProject {
-
-   private projectFilePath: string;
-   private projectSchema: string;
-   private vstSDKDirPath: string;
-
-   constructor(projectDirPath: string) {
-      super(projectDirPath)
-      this.projectFilePath = Path.join(this.projectDirPath, "VST3NetSend.xcodeproj")
-      this.projectSchema = "VST3NetSend-macOS"
-      this.vstSDKDirPath = projectDirPath + "/Vendor/Steinberg"
+class Project extends AbstractProject {
+   constructor(projectDirPath) {
+      super(projectDirPath);
+      this.projectFilePath = Path.join(this.projectDirPath, 'VST3NetSend.xcodeproj');
+      this.projectSchema = 'VST3NetSend-macOS';
+      this.vstSDKDirPath = projectDirPath + '/Vendor/Steinberg';
    }
 
    actions() {
-      return ["ci", "build", "clean", "test", "release", "verify", "deploy", "archive"]
+      return ['ci', 'build', 'clean', 'test', 'release', 'verify', 'deploy', 'archive'];
    }
 
    build() {
-      new WL.XcodeBuilder(this.projectFilePath).build(this.projectSchema)
+      new XcodeBuilder(this.projectFilePath).build(this.projectSchema);
    }
 
    clean() {
-      WL.FileSystem.rmdirIfExists(`${this.projectDirPath}/DerivedData`)
-      WL.FileSystem.rmdirIfExists(`${this.projectDirPath}/Build`)
+      FileSystem.rmdirIfExists(`${this.projectDirPath}/DerivedData`);
+      FileSystem.rmdirIfExists(`${this.projectDirPath}/Build`);
    }
 
    ci() {
-      this.prepare()
-      new WL.XcodeBuilder(this.projectFilePath).ci(this.projectSchema)
+      this.prepare();
+      new XcodeBuilder(this.projectFilePath).ci(this.projectSchema);
    }
 
-   private prepare() {
-      WL.puts("→ Downloading dependencies...")
-      FileSystem.mkdirSync(this.vstSDKDirPath, { recursive: true });
-      process.chdir(this.vstSDKDirPath)
-      WL.execute(`git clone --branch vstsdk368_08_11_2017_build_121 https://github.com/steinbergmedia/vst3sdk.git`)
-      process.chdir(Path.join(this.vstSDKDirPath, "vst3sdk"))
-      WL.execute(`git submodule update --init base pluginterfaces public.sdk`)
+   prepare() {
+      puts('→ Downloading dependencies...');
+      fs.mkdirSync(this.vstSDKDirPath, { recursive: true });
+      process.chdir(this.vstSDKDirPath);
+      execute(`git clone --branch vstsdk368_08_11_2017_build_121 https://github.com/steinbergmedia/vst3sdk.git`);
+      process.chdir(Path.join(this.vstSDKDirPath, 'vst3sdk'));
+      execute(`git submodule update --init base pluginterfaces public.sdk`);
    }
-
 }
