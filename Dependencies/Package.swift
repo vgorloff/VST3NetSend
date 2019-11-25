3d40150d7ef7ee9d0836a705c279e2481130141c
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2019-11-25 23:43:49 +0100

    Dependencies update.

--- Dependencies/Package.swift
+++ Dependencies/Package.swift
@@ -14,14 +14,16 @@ pkg.products = [
 ]
 
 pkg.dependencies = [
-    .package(path: "../../Shared/mcConcurrency"),
-    .package(path: "../../Shared/mcFoundation"),
-    .package(path: "../../Shared/mcMedia")
+    .package(url: "https://bitbucket.org/vgorloff/mcConcurrencyLocking.git", .exact("1.0.0")),
+    .package(url: "https://bitbucket.org/vgorloff/mcFoundationExtensions.git", .exact("1.0.1")),
+    .package(url: "https://bitbucket.org/vgorloff/mcFoundationLogging.git", .exact("1.0.1")),
+    .package(url: "https://bitbucket.org/vgorloff/mcFoundationFormatters.git", .exact("1.0.0")),
+    .package(url: "https://bitbucket.org/vgorloff/mcMediaExtensions.git", .exact("1.0.1")),
+    .package(url: "https://bitbucket.org/vgorloff/mcMediaAU.git", .exact("1.0.0"))
 ]
 
 pkg.targets = [
    .target(name: "VST3NetSendShared",
-           dependencies: ["mcConcurrencyLocking", "mcFoundationExtensions", "mcFoundationLogging", "mcFoundationFormatters",
-                          "mcMediaExtensions", "mcMediaAU"],
+           dependencies: ["mcConcurrencyLocking", "mcFoundationExtensions", "mcFoundationLogging", "mcFoundationFormatters", "mcMediaExtensions", "mcMediaAU"],
            path: "Sources"),
 ]
