d73efd36ca0aabe4e8429de1dcb25e840dc3d420
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2019-11-26 08:38:04 +0100

    Autocorrection.

--- Dependencies/Package.swift
+++ Dependencies/Package.swift
@@ -5,25 +5,17 @@ import PackageDescription
 
 let pkg = Package(name: "VST3NetSendShared")
 
-pkg.platforms = [
-   .macOS(.v10_13)
-]
+pkg.platforms = [.macOS(.v10_13)]
 
-pkg.products = [
-   .library(name: "VST3NetSendShared", targets: ["VST3NetSendShared"])
-]
+pkg.products = [.library(name: "VST3NetSendShared", targets: ["VST3NetSendShared"])]
 
-pkg.dependencies = [
-    .package(url: "https://bitbucket.org/vgorloff/mcConcurrencyLocking.git", .exact("1.0.0")),
-    .package(url: "https://bitbucket.org/vgorloff/mcFoundationExtensions.git", .exact("1.0.1")),
-    .package(url: "https://bitbucket.org/vgorloff/mcFoundationLogging.git", .exact("1.0.1")),
-    .package(url: "https://bitbucket.org/vgorloff/mcFoundationFormatters.git", .exact("1.0.0")),
-    .package(url: "https://bitbucket.org/vgorloff/mcMediaExtensions.git", .exact("1.0.1")),
-    .package(url: "https://bitbucket.org/vgorloff/mcMediaAU.git", .exact("1.0.0"))
-]
+pkg.dependencies = [.package(url: "https://bitbucket.org/vgorloff/mcConcurrencyLocking.git", .exact("1.0.0")),
+                    .package(url: "https://bitbucket.org/vgorloff/mcFoundationExtensions.git", .exact("1.0.1")),
+                    .package(url: "https://bitbucket.org/vgorloff/mcFoundationLogging.git", .exact("1.0.1")),
+                    .package(url: "https://bitbucket.org/vgorloff/mcFoundationFormatters.git", .exact("1.0.0")),
+                    .package(url: "https://bitbucket.org/vgorloff/mcMediaExtensions.git", .exact("1.0.1")),
+                    .package(url: "https://bitbucket.org/vgorloff/mcMediaAU.git", .exact("1.0.0"))]
 
-pkg.targets = [
-   .target(name: "VST3NetSendShared",
-           dependencies: ["mcConcurrencyLocking", "mcFoundationExtensions", "mcFoundationLogging", "mcFoundationFormatters", "mcMediaExtensions", "mcMediaAU"],
-           path: "Sources"),
-]
+pkg.targets = [.target(name: "VST3NetSendShared",
+                       dependencies: ["mcConcurrencyLocking", "mcFoundationExtensions", "mcFoundationLogging", "mcFoundationFormatters", "mcMediaExtensions", "mcMediaAU"],
+                       path: "Sources")]
