89d552618d0159f1c88560eb89d7f75235ee1a1b
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2020-02-08 00:04:11 +0100

    Scripts update.

--- Scripts/archive.sh
+++ Scripts/archive.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Archive: Started..."
 source $(dirname "$0")/common.sh
 
 SrcDirPath=$(cd "$(dirname "$0")/../"; pwd)
 cd "$SrcDirPath"
 
 xcodebuild -project "$AppProjectFilePath" -scheme "$AppSchema" -archivePath "$AppArchiveRoot" archive | xcpretty || exit 1
-cd "$AppArchiveRoot/Products/Library/Audio/Plug-Ins/VST3/WaveLabs" && zip -q --symlinks -r VST3NetSend.vst3.zip VST3NetSend.vst3
+cd "$AppArchiveRoot/Products/Library/Audio/Plug-Ins/VST3/WaveLabs" && zip -q --symlinks -r VST3NetSend.vst3.zip VST3NetSend.vst3 || exit 1
+
+echo "Archive: Done!"
