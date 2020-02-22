1f0b291cd8f8f960c0864391f7a2f186e2b90fd4
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2020-02-22 20:52:21 +0100

    update.

--- Dependencies/Package.swift
+++ Dependencies/Package.swift
@@ -10,12 +10,12 @@ pkg.platforms = [.macOS(.v10_13)]
 pkg.products = [.library(name: "VST3NetSendShared", targets: ["VST3NetSendShared"])]
 
 pkg.dependencies = [
-   .package(url: "https://bitbucket.org/vgorloff/mcConcurrencyLocking.git", .upToNextMinor(from: "1.0.1")),
-   .package(url: "https://bitbucket.org/vgorloff/mcFoundationExtensions.git", .upToNextMinor(from: "1.0.7")),
-   .package(url: "https://bitbucket.org/vgorloff/mcFoundationLogging.git", .upToNextMinor(from: "1.0.4")),
-   .package(url: "https://bitbucket.org/vgorloff/mcFoundationFormatters.git", .upToNextMinor(from: "1.0.3")),
-   .package(url: "https://bitbucket.org/vgorloff/mcMediaExtensions.git", .upToNextMinor(from: "1.0.2")),
-   .package(url: "https://bitbucket.org/vgorloff/mcMediaAU.git", .upToNextMinor(from: "1.0.1"))
+   .package(path: "../node_modules/mc-concurrency-locking"),
+   .package(path: "../node_modules/mc-foundation-extensions"),
+   .package(path: "../node_modules/mc-foundation-formatters"),
+   .package(path: "../node_modules/mc-foundation-logging"),
+   .package(path: "../node_modules/mc-media-au"),
+   .package(path: "../node_modules/mc-media-extensions")
 ]
 
 pkg.targets = [
