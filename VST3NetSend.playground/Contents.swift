df9ff218592e2ae62cba260d0808a6109d53b138
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2020-04-01 22:22:12 +0200

    Added playground.

---
+++ VST3NetSend.playground/Contents.swift
@@ -0,0 +1,10 @@
+import AppKit
+import Foundation
+import Combine
+import PlaygroundSupport
+import SwiftUI
+@testable import VST3NetSendKit
+
+let vc = NetSendViewController()
+
+PlaygroundPage.current.setLiveView(vc)
