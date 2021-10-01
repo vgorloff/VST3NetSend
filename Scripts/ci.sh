2d3b48cbe59e95f5850c3f112d378a14ef80c09f
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2021-10-01 22:11:45 +0200

    Attempt to fix CI.

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -10,11 +10,11 @@ ci_prepare() {
    rm -rf "$AppVSTSDKDirPath"
    mkdir -p "$AppVSTSDKDirPath"
    cd "$AppVSTSDKDirPath"
-   git clone --branch vstsdk368_08_11_2017_build_121 https://github.com/steinbergmedia/vst3sdk.git || exit 1
+   git clone --branch v3.7.3_build_20 https://github.com/steinbergmedia/vst3sdk.git || exit 1
    cd "$AppVSTSDKDirPath/vst3sdk"
    git submodule update --init base pluginterfaces public.sdk || exit 1
 }
 
 ci_prepare
-xcodebuild -project "$AppProjectFilePath" -scheme "$AppSchema" CODE_SIGNING_REQUIRED=NO CODE_SIGN_STYLE=Manual DEVELOPMENT_TEAM= CODE_SIGN_IDENTITY= build
+xcodebuild -quiet -project "$AppProjectFilePath" -scheme "$AppSchema" CODE_SIGNING_REQUIRED=NO CODE_SIGN_STYLE=Manual DEVELOPMENT_TEAM= CODE_SIGN_IDENTITY= MCA_VST_SDK_PATH=Vendor/Steinberg build
 
